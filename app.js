a52a8e74430cfb082787223bea1c2f5eb6db9350
ziakazemi35 <55606252+ziakazemi35@users.noreply.github.com>
2020-01-13 15:41:57 +0100

    background change and added some texts

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// READ MORE BUTTON FOR PARAGRAPHS OF SNOOKER RULES 
 $('.btn-readmore').on('click', function () {
   $this = $(this);
   $this.parent().parent().find(".read-more-container").toggleClass('read-more-hidden');
   $this.parent().find(".btn-readmore").toggleClass('read-more-hidden');
 });
+
+
+// READ MORE BUTTON FOR PARAGRAPHS OF POOL RULES 
+$('.btn-pool-readmore').on('click', function () {
+  $this = $(this);
+  $this.parent().parent().find(".read-more-container").toggleClass('read-more-hidden');
+  $this.parent().find(".btn-readmore").toggleClass('read-more-hidden');
+});
+
 
 
